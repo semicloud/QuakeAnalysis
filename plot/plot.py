49873c2d971c15a6f2181f5a6dd7dfef357b5c61
semicloud <semicloud@163.com>
2019-05-30 12:53:55 +0800

    Add read shape file (Line and Point) to plot.py, add text (after filter) to the figure

--- plot/plot.py
+++ plot/plot.py
@@ -9,6 +9,10 @@ max_lon = 140
 min_lat = 20
 max_lat = 50
 
+fault_shp = r"E:\modis_workspace_test\faults\faults_deng_simple"
+block_shp = r"E:\modis_workspace_test\faults\China_block3"
+city_shp = r"E:\modis_workspace_test\faults\cities"
+
 lon_0 = (min_lon + max_lon) / 2
 lat_0 = (min_lat + max_lat) / 2
 
@@ -21,7 +25,33 @@ m.drawcoastlines()
 m.drawcountries()
 m.drawparallels(np.arange(0, 81, 10), labels=[True, False, False, False])
 m.drawmeridians(np.arange(10., 351, 10), labels=[False, False, False, True])
+
 # m.drawmapboundary(fill_color='aqua')
 # m.fillcontinents(color='coral', lake_color='aqua')
+
+
+# Add shapefile
+# m.readshapefile(fault_shp, name='', linewidth=1, color='red')  # name can be empty for line shapefile
+m.readshapefile(block_shp, name='', linewidth=1, color='blue')
+m.readshapefile(city_shp, name='city', linewidth=5, color='green')
+
+print('before filter')
+print(len(m.city))
+print(m.city)
+
+# filter shape file, otherwise plt.text will plot all text in the file!
+# in projection cyl, city_pos is lonlat, other projection does not work
+# may be need transform before filter
+city_info, city_pos = zip(*[(info, pos) for info, pos in zip(m.city_info, m.city) if
+                            min_lon < pos[0] < max_lon and min_lat < pos[1] < max_lat])
+
+print('after filter')
+print(len(city_info))
+
+for info, pos in zip(city_info, city_pos):
+    m.plot(pos[0], pos[1], color='green', marker='o', markersize=4)
+    xpt, ypt = m(pos[0], pos[1])
+    plt.text(xpt + 0.2, ypt + 0.2, info["NAME"])
+
 plt.title('Title title title title')
 plt.show()
